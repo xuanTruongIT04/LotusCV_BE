3df35291e9823d9d4dbf8238188046e75c9ef5f9
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2024-03-31 22:24:04 +0700

    Validation data & custom message

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +12,8 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(configService.get<string>("PORT"));
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,20 @@
-export class CreateUserDto {
-    email: string;
-    password: string;
-    name: string;
-    address: string;
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
+export class CreateUserDto {
+  @IsNotEmpty({
+    message: 'Email is not blank',
+  })
+  @IsEmail({}, {
+    message: 'Email is incorrect format',
+  })
+  email: string;
+
+  @IsNotEmpty({
+    message: 'Password is not blank',
+  })
+  password: string;
+
+  name: string;
+
+  address: string;
 }
