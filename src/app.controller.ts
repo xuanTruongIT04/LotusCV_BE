b7a324b9fb29920cdb6ab7db58f0346385ca70bf
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2024-04-01 21:47:14 +0700

    Use JWT create access token when user login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,17 +2,19 @@ import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     public configServie: ConfigService,
+    private authService: AuthService
   ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   async handleLogin(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
