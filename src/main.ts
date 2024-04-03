babead8897a55e6fc204d702e586407aa9e6f32f
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2024-04-03 17:02:21 +0700

    Transform response

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,25 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
 
+  // Config Guards
   const reflector: Reflector = new Reflector();
   app.useGlobalGuards(new JwtAuthGuard(reflector));
-  
+
+  // Config Interceptor
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   //Config CORS
-  app.enableCors(
-    {
-      "origin": "*",
-      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-      "preflightContinue": false,
-    }
-  );
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
