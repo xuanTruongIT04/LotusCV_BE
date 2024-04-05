e718e50f879c69259b50578aed222de9928faafb
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2024-04-05 14:12:45 +0700

    Update model user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,37 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsDefined,
+  IsEmail,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  ValidateNested,
+} from 'class-validator';
+import mongoose from 'mongoose';
+
+class Company {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty()
+  name: string;
+}
 
 export class CreateUserDto {
   @IsNotEmpty({
+    message: 'Name is not blank',
+  })
+  name: string;
+
+  @IsNotEmpty({
     message: 'Email is not blank',
   })
-  @IsEmail({}, {
-    message: 'Email is incorrect format',
-  })
+  @IsEmail(
+    {},
+    {
+      message: 'Email is incorrect format',
+    },
+  )
   email: string;
 
   @IsNotEmpty({
@@ -14,7 +39,68 @@ export class CreateUserDto {
   })
   password: string;
 
+  @IsNotEmpty({
+    message: 'Address is not blank',
+  })
+  address: string;
+
+  @IsNotEmpty({
+    message: 'Age is not blank',
+  })
+  age: number;
+
+  @IsNotEmpty({
+    message: 'Gender is not blank',
+  })
+  gender: string;
+
+  @IsNotEmpty({
+    message: 'Role is not blank',
+  })
+  role: string;
+
+  @IsDefined()
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
+}
+
+export class RegisterUserDto {
+  @IsNotEmpty({
+    message: 'Name is not blank',
+  })
   name: string;
 
+  @IsNotEmpty({
+    message: 'Email is not blank',
+  })
+  @IsEmail(
+    {},
+    {
+      message: 'Email is incorrect format',
+    },
+  )
+  email: string;
+
+  @IsNotEmpty({
+    message: 'Password is not blank',
+  })
+  password: string;
+
+  @IsNotEmpty({
+    message: 'Address is not blank',
+  })
   address: string;
+
+  @IsNotEmpty({
+    message: 'Age is not blank',
+  })
+  age: number;
+
+  @IsNotEmpty({
+    message: 'Gender is not blank',
+  })
+  gender: string;
 }
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,10 +1,13 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+                                                      import mongoose, { HydratedDocument, Mongoose } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
+  @Prop()
+  name: string;
+
   @Prop({ required: true })
   email: string;
 
@@ -12,16 +15,43 @@ export class User {
   password: string;
 
   @Prop()
-  name: string;
-
-  @Prop()
-  phone: string;
-
-  @Prop()
   age: number;
 
   @Prop()
+  gender: string;
+
+  @Prop()
   address: string;
+
+  @Prop({ type: Object })
+  company: {
+    _id: mongoose.Schema.Types.ObjectId;
+    name: string;
+  };
+
+  @Prop()
+  role: string;
+
+  @Prop()
+  refreshToken: string;
+
+  @Prop({ type: Object })
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
 
   @Prop()
   createdAt: Date;
