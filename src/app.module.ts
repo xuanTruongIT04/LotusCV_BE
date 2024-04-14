324c6e6f33295264f9ab0dde1e0f604b8555e5b9
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2024-04-14 13:35:27 +0700

    Feature healthchecks

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,6 +17,7 @@ import { SubscribersModule } from './subscribers/subscribers.module';
 import { MailModule } from './mail/mail.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -53,6 +54,7 @@ import { ThrottlerModule } from '@nestjs/throttler';
     DatabasesModule,
     SubscribersModule,
     MailModule,
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/health/health.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  MongooseHealthIndicator,
+} from '@nestjs/terminus';
+import { Public } from 'src/decorator/customize';
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private db: MongooseHealthIndicator,
+  ) {}
+  
+  @Get()
+  @Public()
+  @HealthCheck()
+  check() {
+    return this.health.check([() => this.db.pingCheck('database')]);
+  }
+}
